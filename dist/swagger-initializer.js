33134847cced1853b5552a8e813b4fe526dadd00
Leor Seligmann <leors@vatitstream.com>
2022-04-25 14:48:43 +0200

    fix

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://reseller-api-docs.vatit-stream.com/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
